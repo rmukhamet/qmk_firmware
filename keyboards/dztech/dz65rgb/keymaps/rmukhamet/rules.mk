cad6dfe597da6c5aad56cf6863f3727a826424c3
Rafael Mukhametdzhanov <rafael.mukhametdzhanov@acronis.com>
2022-06-06 11:36:15 +0300

    first version, no RGB, second layer with issues

---
+++ keyboards/dztech/dz65rgb/keymaps/rmukhamet/rules.mk
@@ -0,0 +1,14 @@
+# Userspace defines
+RGB_MATRIX_ENABLE = no # Disable the ugly RGB light
+
+BACKLIGHT_ENABLE = no
+
+
+KEY_LOCK_ENABLE = no
+
+
+RGBLIGHT_ENABLE = no # additional backlight
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+GRAVE_ESC_ENABLE = no
+
+SPLIT_KEYBOARD    = no
