124577dba5232ae3bcaa9b12479bb69bb0d2fe9e
Rafael Mukhametdzhanov <rafael.mukhametdzhanov@acronis.com>
2022-06-26 22:05:47 +0300

    fix layout

--- keyboards/dztech/dz65rgb/keymaps/rmukhamet/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/rmukhamet/keymap.c
@@ -14,26 +14,33 @@ enum layers {
 44,  45,  46,  47,  48,  49,  50,  51,  52,  53,  54,  55,       56,  57
 58,  59,  60,            61,                 62,  63,  64,  65,  66,  67
 
-[KEYS]
+[_BL]
 ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   -,   =,  BCK,HOME
 TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   [,   ],    \,PGUP
 CPS, A,   S,   D,   F,   G,   H,   J,   K,   L,   COL, QOT,  RETURN,PGDN
 SFT, Z,   X,   C,   V,   B,   N,   M,   COM, DOT, SLS, SHIFT,    UP, DEL
-CTL, GUI, ALT,           SPACEBAR,           ALT, FN, CTL, LFT, DWN, RIT
+CTL, ALT, GUI,           SPACEBAR,           ALT, _FL, CTL, LFT, DWN, RIT
+
+[_FL]
+ESC,       F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12,   DEL ,HOME
+TAB,        Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   [,   ],QK_BOOT,PGUP
+CPS,        A,   S,   D,   F,   G,   H,   J,   K,   L,   COL, QOT,    RETURN,PGDN
+SFT,        Z,   X,   C,   V,   B,   N,   M,   COM, DOT, SLS, SHIFT,      UP, DEL
+CTL,        GUI, ALT,           SPACEBAR,           ALT, FN, CTL, LFT,   DWN, RIT
 */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT_65_ansi(
-        KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
+        KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
-        LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,
-        KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,  RESET /*KC_END*/,
+        LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT, KC_PGDN,
+        KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,   KC_UP,  KC_END, 
         KC_LCTL,    KC_LALT,   KC_LGUI,                            KC_SPC,                    KC_RALT, MO(_FL),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_FL] = LAYOUT_65_ansi(
-        KC_GESC,        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_HOME,
-        _______,        RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, KC_PSCR, KC_SLCK, KC_PAUS, QK_BOOT, KC_PGUP,
-        CTL_T(KC_CAPS), RGB_SPI, RGB_SPD, _______, _______, _______, _______, _______, _______, _______, _______, _______,          EEP_RST, KC_PGDN,
-        KC_LSFT,                 _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______, _______, KC_VOLU, KC_MUTE,
+        _______,        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______,
+        _______,        RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, KC_PSCR, KC_SLCK, KC_PAUS, QK_BOOT, _______,
+        CTL_T(KC_CAPS), RGB_SPI, RGB_SPD, _______, _______, _______, _______, _______, _______, _______, _______, _______,          EE_CLR , _______,
+        _______,                 _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______, _______, KC_VOLU, KC_MUTE,
         _______,        _______, _______,                            _______,                   RESET,  _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
     )
 };
