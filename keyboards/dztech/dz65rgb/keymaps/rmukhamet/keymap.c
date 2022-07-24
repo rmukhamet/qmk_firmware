bf517bcb6c780df50aa02c66145568e9318fdd6b
Rafael Mukhametdzhanov <rafael.mukhametdzhanov@acronis.com>
2022-07-24 11:49:27 +0300

    define ctrl space key

--- keyboards/dztech/dz65rgb/keymaps/rmukhamet/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/rmukhamet/keymap.c
@@ -5,6 +5,10 @@ enum layers {
     _FL,
     _ML
 };
+
+#define CTRL_SPACE LCTL(KC_SPACE)
+
+
 /**************** LAYOUT *********************/
 
 /*
@@ -63,28 +67,94 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                KC_ENT , KC_WH_D,
-        XXXXXXX,                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN1, KC_MS_U, KC_BTN2,
+        CTRL_SPACE,                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN1, KC_MS_U, KC_BTN2,
         _______,        XXXXXXX, XXXXXXX,                            XXXXXXX,                   XXXXXXX, MO(_FL), XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R
     )
 };
 
+void matrix_init_user(void) {
 
+}
 
-
-const key_override_t alt_space_override = ko_make_with_layers_negmods_and_options(
-    MOD_MASK_ALT,   // Trigger modifiers: alt 
-    KC_SPACE,       // Trigger key: 						
-    LCTL(KC_SPACE), // Replacement key
-    _BL,            // Activate on layer
-    MOD_MASK_CS,    // Do not activate when shift or ctrl are pressed
-    ko_option_no_reregister_trigger // Specifies that the play key is not registered again after lifting ctrl
+const key_override_t alt_space_override = ko_make_with_layers(
+    KC_LALT,   // Trigger modifiers: alt 
+    KC_SPACE,       // Trigger key: space						
+    CTRL_SPACE, // Replacement key
+    ~0           // Activate on layer
+    //MOD_MASK_SHIFT,    // Do not activate when shift or ctrl are pressed
+  //  ko_option_no_reregister_trigger // Specifies that the space key is not registered again after lifting alt
 ); 
-
-
-
+const key_override_t alt_space_override_advanched = ko_make_with_layers_negmods_and_options(
+    KC_TAB,   // Trigger modifiers: alt 
+    KC_SPACE,       // Trigger key: space						
+    CTRL_SPACE, // Replacement key
+    _BL,           // Activate on layer
+    MOD_MASK_SHIFT,    // Do not activate when shift or ctrl are pressed
+    ko_option_no_reregister_trigger // Specifies that the space key is not registered again after lifting alt
+); 
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
 	&alt_space_override,
+    &delete_key_override,
+    &alt_space_override_advanched,
     NULL
 };
+
+
+// switch layers depending on keypressed, with persistent layer in the mix
+//static bool bsdel_mods = false;
+
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//     switch (keycode) {
+//         case KC_BSPC: {
+//             uint8_t kc = KC_BSPC;
+
+//             if (keyboard_report->mods) {
+//                 kc         = KC_DEL;
+//                 bsdel_mods = true;
+//             } else {
+//                 bsdel_mods = false;
+//             }
+
+//             if (record->event.pressed)
+//                 register_code(kc);
+//             else
+//                 unregister_code(kc);
+//         }
+//         default:
+//             break;
+//     }
+//     return true;
+// }
+
+// second attemp
+// static uint8_t saved_mods = 0;
+
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//     switch (keycode) {
+//         case KC_BSPC: {
+//             // uint8_t kc = KC_BSPC;
+//             if (record->event.pressed) {
+//                 if (get_mods() & MOD_MASK_SHIFT) {
+//                     saved_mods = get_mods() & MOD_MASK_SHIFT;
+//                     del_mods(saved_mods);
+//                     register_code(KC_DEL);
+//                     unregister_code(KC_BSPC);
+
+//                 } else {
+//                     saved_mods = 0;
+//                     register_code(KC_BSPC);
+//                 }
+//             } else {
+//                 add_mods(saved_mods);
+//                 unregister_code(KC_BSPC);
+//                 unregister_code(KC_DEL);
+//             }
+//         }
+//         default:
+//             break;
+//     }
+//     return true;
+// }
